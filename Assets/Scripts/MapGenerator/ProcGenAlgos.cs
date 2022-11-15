a65f157d39b7490fe9c8af392f45d9ca83fa8004
Adrien Philippon <57765484+adrimyla@users.noreply.github.com>
2022-11-15 16:33:33 +0100

    Creating ProcGenAlgos script
    Creating a class containing the method SimpleRandomWalk

---
+++ Assets/Scripts/MapGenerator/ProcGenAlgos.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProcGenAlgos
+{
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+        var previousPosition = startPosition;
+
+        for(int i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
+            path.Add(newPosition);
+            previousPosition = newPosition;
+        }
+
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    //Contains different direction on a 2D map
+    public static List<Vector2Int> cardinalDirectiobnList = new List<Vector2Int>
+    {
+        new Vector2Int(0,1), //UP
+        new Vector2Int(1,0), //RIGHT
+        new Vector2Int(0,-1), //DOWN
+        new Vector2Int(-1,0), //LEFT
+    };
+
+    //Return a random direction
+    public static Vector2Int GetRandomCardinalDirection()
+    {
+        return cardinalDirectiobnList[Random.Range(0, cardinalDirectiobnList.Count)];
+    }
+}
