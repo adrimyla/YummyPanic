14dd1ba4339c8498fce82903108c31f0229487d2
Adrien Philippon <57765484+adrimyla@users.noreply.github.com>
2022-11-25 10:34:53 +0100

    Adding some text

--- Assets/Scripts/GameManagerScripts/GameManager.cs
+++ Assets/Scripts/GameManagerScripts/GameManager.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Game Manager")]
     public static GameManager Instance;
     public GameState state;
     public static event Action<GameState> OnGameStateChanged;
+
+    [Header("Scene")]
+    public TilemapVisualizer tilemapVisualizer;
+    public GameObject grid;
+
+    [Header("Canvas")]
+    public GameObject mainMenuCanvas;
+
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Gluttons")]
+    public GameObject gluttonPrefab;
 
     void Awake()
     {
@@ -16,11 +30,12 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        UpdateGameState(GameState.INTRO);
+        UpdateGameState(state);
     }
 
     public void UpdateGameState(GameState newState)
     {
+        Debug.Log("<color=orange>[GAME STATE] : " + newState + "</color>");
         state = newState;
 
         switch (newState)
@@ -30,6 +45,9 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.MAIN_MENU:
                 HandleMainMenu();
+                break;
+            case GameState.LOADING:
+                HandleLoading();
                 break;
             case GameState.PLAYING:
                 HandlePlayMode();
@@ -47,17 +65,7 @@ public class GameManager : MonoBehaviour
         OnGameStateChanged(newState);
     }
 
-    private void HandleGameOver()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void HandlePauseMode()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void HandlePlayMode()
+    private void HandleIntro()
     {
         throw new NotImplementedException();
     }
@@ -67,17 +75,54 @@ public class GameManager : MonoBehaviour
         throw new NotImplementedException();
     }
 
-    private void HandleIntro()
+    private void HandleLoading()
+    {
+        Debug.Log("Loading game ...");
+
+        //STEP 1 : Creating tile maps and assigning it to TileMapVisualizer
+
+        //STEP  2 : Generating dungeon and kitchen
+
+        //STEP 3 : Placing player at spawn
+
+        //STEP 4 : Placing gluttons on map
+
+        //STEP 5 : Placing food, fake food and objects on map
+
+        //STEP 6 : Placing gluttons burrows (terriers)
+
+        //STEP 8 : Init player and gluttons stats (use default values of prefab)
+
+        //STEP 9 : Init receips and countdown 
+
+        UpdateGameState(GameState.PLAYING); //When loading complete, we start the game !
+    }
+
+    private void HandlePlayMode()
+    {
+        Debug.Log("Playing !");
+        throw new NotImplementedException();
+    }
+
+    private void HandlePauseMode()
     {
         throw new NotImplementedException();
     }
-}
 
+    private void HandleGameOver()
+    {
+        throw new NotImplementedException();
+    }
+
+}
 public enum GameState
 {
     INTRO,
     MAIN_MENU,
+    LOADING,
     PLAYING,
     PAUSE,
     GAME_OVER
 }
+
+
