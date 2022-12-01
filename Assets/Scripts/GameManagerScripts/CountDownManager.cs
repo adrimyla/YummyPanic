90006185f4a00bf80d7fb6dea89acd152a0980a3
Adrien Philippon <57765484+adrimyla@users.noreply.github.com>
2022-12-01 11:03:45 +0100

    Changing countdown to 60 seconds

--- Assets/Scripts/GameManagerScripts/CountDownManager.cs
+++ Assets/Scripts/GameManagerScripts/CountDownManager.cs
@@ -49,6 +49,7 @@ public class CountDownManager
     }
     void DisplayTime(float timeToDisplay)
     {
+        timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         countDownDisplayerText.GetComponent<TextMeshProUGUI>().text = string.Format("{0:00}:{1:00}", minutes, seconds);
