ad88adfd40144ac6e18908cca1a0b8379a95faf3
DMR <77438997+Dina-MR@users.noreply.github.com>
2022-12-01 11:07:50 +0100

    Improving MeshNav & adding a state machine to the enemies

--- Assets/Scripts/EnemiesScripts/GluttonAI.cs
+++ Assets/Scripts/EnemiesScripts/GluttonAI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
@@ -7,34 +8,78 @@ using UnityEngine.SceneManagement;
 public class GluttonAI : MonoBehaviour
 {
     private NavMeshAgent thisAgent = null;
-    public Transform target = null; 
-    [SerializeField] private string _targetedFoodTag;
+    public Transform target = null;
+    public Transform homeLocation;
+    public string targetedFoodTag;
     [SerializeField] private float _distanceForEating;
     [SerializeField] private float _speed;
-    private GameObject currentTarget = null;
+    public GameObject currentTarget = null;
+
+    [Header("Roaming parameters")]
+    [SerializeField] private float _roamingRadius;
+    [SerializeField] private int _randomPositionsCount;
+    [SerializeField] private float _timerBeforeNextPosition;
+    private Vector3 _currentRoamingPosition = Vector3.zero;
+    private float _timer;
 
     private void Awake()
     {
         thisAgent = GetComponent<NavMeshAgent>();
+        // Prevent the Glutton from being impacted by the Z axis
+        thisAgent.updateRotation = false;
+        thisAgent.updateUpAxis = false;
+        // Set timer to start the random roaming
+        _timer = _timerBeforeNextPosition;
     }
 
     private void Update()
     {
-        thisAgent.SetDestination(target.position);
+        //if(currentTarget != null)
+        //    thisAgent.SetDestination(target.position);
+    }
+
+    public void MoveRandomly() 
+    {
+        _timer += Time.deltaTime;
+        if(_timer >= _timerBeforeNextPosition || transform.position == _currentRoamingPosition)
+        {
+            _currentRoamingPosition = (Vector2)transform.position + Random.insideUnitCircle * _roamingRadius;
+            MoveTowardTarget(_currentRoamingPosition);
+            _timer = 0f;
+        }
+        else
+        {
+            Debug.Log("Nope");
+        }
+    }
+
+    public void MoveTowardTarget(Vector3 destination)
+    {
+        thisAgent.SetDestination(destination);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(_targetedFoodTag))
+        if (collision.gameObject.CompareTag(targetedFoodTag) && currentTarget == null)
         {
             currentTarget = collision.gameObject;
             Debug.Log("Nourriture détectée");
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(targetedFoodTag) && currentTarget != null && Vector3.Distance(collision.transform.position, this.transform.position) <= _distanceForEating)
+        {
+            Destroy(collision.gameObject);
+            currentTarget = null;
+            Debug.Log("Nourriture récupérée");
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(_targetedFoodTag) && currentTarget != null)
+        if (collision.gameObject.CompareTag(targetedFoodTag) && currentTarget != null)
         {
             currentTarget = null;
         }
